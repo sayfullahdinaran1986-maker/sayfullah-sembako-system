48e25f31fef5d0d72e5d7a12af5562764e78a437
sayfullahdinaran1986-maker <sayfullah.dinaran1986@gmail.com>
2026-01-30 20:28:50 +0800

    Update rekap.js

--- js/rekap.js
+++ js/rekap.js
@@ -1,34 +1,16 @@
 import { db } from "./firebase.js";
-import {
-  collection,
-  getDocs
-} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-const table = document.getElementById("rekap");
-
-const customers = await getDocs(collection(db,"customers"));
+const table=document.getElementById("rekap");
+const customers=await getDocs(collection(db,"customers"));
 
 for(const c of customers.docs){
-  const cid = c.id;
-  const name = c.data().name;
-
-  let total = 0;
-  let paid = 0;
-
-  const purchases = await getDocs(collection(db,"customers",cid,"purchases"));
-  purchases.forEach(p=> total += p.data().total );
-
-  const payments = await getDocs(collection(db,"customers",cid,"payments"));
-  payments.forEach(p=> paid += p.data().amount );
-
-  const sisa = total - paid;
-
-  table.innerHTML += `
-    <tr onclick="location.href='invoice.html?id=${cid}'">
-      <td>${name}</td>
-      <td>Rp ${total.toLocaleString()}</td>
-      <td>Rp ${paid.toLocaleString()}</td>
-      <td><b>Rp ${sisa.toLocaleString()}</b></td>
-    </tr>
-  `;
+ const cid=c.id,name=c.data().name;
+ let total=0,paid=0;
+ const p=await getDocs(collection(db,"customers",cid,"purchases"));
+ p.forEach(x=>total+=x.data().total);
+ const pay=await getDocs(collection(db,"customers",cid,"payments"));
+ pay.forEach(x=>paid+=x.data().amount);
+ table.innerHTML+=`<tr onclick="location.href='invoice.html?id=${cid}'">
+  <td>${name}</td><td>${total}</td><td>${paid}</td><td>${total-paid}</td></tr>`;
 }
