085bb571fde0444c0fe3f8ffcde6b9bab99d7cee
sayfullahdinaran1986-maker <sayfullah.dinaran1986@gmail.com>
2026-01-31 06:11:22 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
- apiKey: "AIzaSyAdk--C9dnZHxElwk56qNzwUI9Pp9_ZDRE",
- authDomain: "sayfullah-sembako-system.firebaseapp.com",
- projectId: "sayfullah-sembako-system",
- storageBucket: "sayfullah-sembako-system.appspot.com",
- messagingSenderId: "123138142607",
- appId: "1:123138142607:web:3f5fadd22f99f5d3731bfa"
+  apiKey: "AIzaSyAdk--C9dnZHxElwk56qNzwUI9Pp9_ZDRE",
+  authDomain: "sayfullah-sembako-system.firebaseapp.com",
+  projectId: "sayfullah-sembako-system",
+  storageBucket: "sayfullah-sembako-system.appspot.com",
+  messagingSenderId: "123138142607",
+  appId: "1:123138142607:web:3f5fadd22f99f5d3731bfa"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
